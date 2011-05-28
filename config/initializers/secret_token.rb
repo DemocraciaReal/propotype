b3b040ad0a97e5fd54218197aa1bc91cd52468ac
Nerian <siotopo@gmail.com>
2011-05-28 19:24:13 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Realdemocracy::Application.config.secret_token = 'd08930247f74f7e2c35fff98588b2f064b55c682af683ed86920d1a08245eed218e8694ed04bca7680b22888e36eeadc28edf7cc089588904b2c8992afab0c18'
