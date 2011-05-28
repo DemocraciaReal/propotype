50160f55bb8aa809c79d4b024ddd52d6401305c5
Nerian <siotopo@gmail.com>
2011-05-28 19:41:45 +0200

    add readme

---
+++ README.mk
@@ -0,0 +1,25 @@
+Read Democracy prototype I
+===========================                                                     
+
+Real democracy in its core meaning is nothing more that this: Each person of a population represents himself and so has the right to vote over all significative decisions in the day to day activity of the government. 
+
+That was an utopia 100 hundred years ago because of practical impediments. It was simply impossible for the entire population of a country – or a significant part of it – to cast a vote many times a week. There was no technology to support that. People would have to travel miles in order to casta  vote and then go back. That would be a nonsense.
+
+But this the XXI century and internet has made possible for people in very distant places to communicate with each other. 
+
+We believe that technology can help to support a real democracy.
+
+This project is a prototype of a software to support  a Real Democracy. The objective of this project is to gain practical knowledge of the problem that we want solve: What features does it need to be implemented in a web software in order to make possible a real democracy – active participation of a large population over the significative decisions that the government makes.
+
+
+Development Guidelines
+==========================
+
+To add features: 
+
+1. Fork
+2. Create a new branch
+3. Create acceptance tests
+4. Make the tests pass
+5. Commit
+6. Send pull request
