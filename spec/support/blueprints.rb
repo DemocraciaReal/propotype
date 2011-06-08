e089264ad859fd785209e9ad193e7ff0852c5904
Nerian <siotopo@gmail.com>
2011-06-08 16:06:58 +0200

    Add blueprint for Proposals

---
+++ spec/support/blueprints.rb
@@ -0,0 +1,16 @@
+require 'machinist/active_record'
+
+# Add your blueprints here.
+#
+# e.g.
+#   Post.blueprint do
+#     title { "Post #{sn}" }
+#     body  { "Lorem ipsum..." }
+#   end
+           
+
+Proposal.blueprint do
+  title{ 'Lorem' }
+  description { 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}  
+end
+
